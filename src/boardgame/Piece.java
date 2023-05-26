ceee11322d6535a49c9bca1f0a30c07ce7c8b91a
Breno Dionisio <brnodionisio23@gmail.com>
2023-05-26 14:21:22 -0300

    refinement and bugfix

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -1,6 +1,7 @@
 package boardgame;
 
 public abstract class Piece {
+
     protected Position position;
     private Board board;
 
@@ -22,8 +23,8 @@ public abstract class Piece {
     public boolean isThereAnyPossibleMove() {
         boolean[][] mat = possibleMoves();
         for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat.length; i++) {
-                if (mat[i][i]) {
+            for (int j = 0; j < mat.length; j++) {
+                if (mat[i][j]) {
                     return true;
                 }
             }
